f4fe213b5bcd6fa6c6962d42b2e33fc58d05a013
Shubham Raj <shubhamraj2202@gmail.com>
2023-04-03 14:51:40 +0900

    Fix Type Annotate (#9)

--- stock_computing_service/statistics/model.py
+++ stock_computing_service/statistics/model.py
@@ -1,7 +1,7 @@
 """ Model class for Statistics """
 from __future__ import annotations
 
-from typing import Any, Optional
+from typing import Any, List, Optional
 
 from pydantic import BaseModel
 
